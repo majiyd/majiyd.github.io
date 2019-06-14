7b051426a122e2c9e263099e87863af3beb9924e
Majiyd <suleimanmajiyd@gmail.com>
2019-06-14 22:27:01 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.eca18f3dd2a0a3c8ec1c46a39645d3cd.js"
 );
 
 self.addEventListener('message', (event) => {
