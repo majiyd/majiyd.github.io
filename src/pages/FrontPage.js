151f843df668578f5db1fa744cc5d3d1d48a3b0d
Majiyd <suleimanmajiyd@gmail.com>
2019-06-23 13:55:33 +0100

    add background image

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -3,9 +3,18 @@ import React from 'react'
 class FrontPage extends React.Component {
   
   render() {
-    return ( 
-      <div>
-
+    return (
+      <div className="frontPage">
+        <h1
+          style={{
+            color: "#cfeccc",
+            textAlign: 'center',
+            position: 'relative',
+            top: '35%'
+          }}
+        >
+          Hi! My name is Majiyd and i'm a developer
+        </h1>
       </div>
     );
   }
