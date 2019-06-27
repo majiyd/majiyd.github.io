8db6c9fa6be579ac88cef32a6bafec8b8e528025
Majiyd <suleimanmajiyd@gmail.com>
2019-06-27 21:59:09 +0100

    change background image and add dark overlay

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -5,17 +5,20 @@ class FrontPage extends React.Component {
   render() {
     return (
       <div className="frontPage">
-        <h1
-          style={{
-            color: "#cfeccc",
-            textAlign: 'center',
-            position: 'relative',
-            top: '35%',
-            fontSize: 40,
-          }}
-        >
-          Hi! My name is Majiyd and i'm a developer
-        </h1>
+        <div className='overlay'>  
+          <h1
+            style={{
+              color: "white",
+              textAlign: 'center',
+              position: 'relative',
+              top: '35%',
+              fontSize: 40,
+              fontWeight: '400'
+            }}
+          >
+            Hi! My name is Majiyd and i'm a developer
+          </h1>
+        </div>
       </div>
     );
   }
