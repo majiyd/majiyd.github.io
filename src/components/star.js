5995aac202a970c3519f0810b79e0f11b07b7d31
Majiyd <suleimanmajiyd@gmail.com>
2019-06-28 09:56:04 +0100

    adjust star twinkling effect

--- src/components/star.js
+++ src/components/star.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 export function Star(props){
+  let width = `${Math.floor(Math.random() * (5 - 2)) + 2}px`
   return (
     <div className='star' style={{
-      width: `${Math.floor(Math.random() * (4 - 2)) + 2}px`,
-      height: `${Math.floor(Math.random() * (4 - 2)) + 2}px`,
+      width: width,
+      height: width,
       position: 'absolute',
       top: `${Math.floor(Math.random() * (100 - 0)) + 0}%`,
       left: `${Math.floor(Math.random() * (100 - 0)) + 0}%`,
       borderRadius: `50%`,
-      animationDuration: `${Math.floor(Math.random() * (5 - 2)) + 2}s`,
-      animationDelay: `${Math.floor(Math.random() * (10 - 1)) + 1}s`,
+      animationDuration: `${Math.random() * (5 - 2) + 2}s`,
+      animationDelay: `${Math.floor(Math.random() * (6 - 1)) + 1}s`,
     }}>
 
     </div>
